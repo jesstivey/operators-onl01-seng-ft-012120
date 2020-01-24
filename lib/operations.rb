c9bdf780f26bb524990f9097d390b69370b84c07
Jessie Tivey <jesstivey@gmail.com>
2020-01-24 20:18:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40 
+    puts true
+  elsif speed > 60
+    puts true
+  else
+    false
+  end   
 end
 
 
 
 def not_safe?(speed)
-	
+ speed < 40 or > 60
 end
 	
 
